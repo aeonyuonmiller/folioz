f8519c25a304d532ee57aa18164a7b79869c17ee
aeonyuonmiller <aym1@mail.com>
2023-06-16 12:55:27 +0200

    changed date format

--- src/app/page.js
+++ src/app/page.js
@@ -1,37 +1,60 @@
 // import Image from 'next/image'
 "use client";
 
-import { motion } from "framer-motion"
-import Logo from './Components/Logo'
+import { motion } from "framer-motion";
+import Logo from "./Components/Logo";
 import Card from "./Components/Card";
 
 export default function Home() {
   return (
     <>
-    <motion.div className='portrait' initial={{y: 50, opacity:0}} animate={{y: 0, opacity:1}} transition={{duration: .4, ease: [.3,0,.2,1.01]}} />
-    
-    <motion.div className='title' initial={{y: 50, opacity:0}} animate={{y: 0, opacity:1}} transition={{duration: .4, ease: [.3,0,.2,1.01], delay: .1}}>Portfolio <span>of</span> Aeon Yuon Miller</motion.div>
+      <motion.div
+        className="portrait"
+        initial={{ y: 50, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.4, ease: [0.3, 0, 0.2, 1.01] }}
+      />
 
+      <motion.div
+        className="title"
+        initial={{ y: 50, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.4, ease: [0.3, 0, 0.2, 1.01], delay: 0.1 }}
+      >
+        Portfolio <span>of</span> Aeon Yuon Miller
+      </motion.div>
 
-    <motion.div className='glas' initial={{y: "10vh", opacity: 0}} animate={{y: "0vh", opacity: 1}} transition={{duration: .4, ease: [.3,0,.2,1.01], delay: 1 }}>
-      <h5>View works</h5>
-      <div className="card-grid">
-        <Card title="Mind your cookies" date="xx.xx.xxxx" />
-        <Card title="my minimalist mac set-up" date="xx.xx.xxxx" />
-        <Card title="5 fav books" date="15.06.2023" />
-        <Card title="Best albums" date="15.06.2023" />
+      <motion.div
+        className="glas"
+        initial={{ y: "10vh", opacity: 0 }}
+        animate={{ y: "0vh", opacity: 1 }}
+        transition={{ duration: 0.4, ease: [0.3, 0, 0.2, 1.01], delay: 1 }}
+      >
+        <h5>View works</h5>
+        <div className="card-grid">
+          <Card title="Mind your cookies" date="15th July '23" />
+          <Card title="my minimalist mac set-up" date="15th July '23" />
+          <Card title="5 fav books" date="15th July '23" />
+          <Card title="Best albums" date="15th July '23" />
 
-        <Card title="my minimalist mac set-up" date="xx.xx.xxxx" />
-        <Card title="Mind your cookies" date="xx.xx.xxxx" />
-        <Card title="my minimalist mac set-up" date="xx.xx.xxxx" />
-        <Card title="My 5 fav books" date="15.06.2023" />
-        <Card title="Best albums" date="15.06.2023" />
-      </div>
-    </motion.div>
+          <Card title="my minimalist mac set-up" date="15th July '23" />
+          <Card title="Mind your cookies" date="15th July '23" />
+          <Card title="my minimalist mac set-up" date="15th July '23" />
+          <Card title="My 5 fav books" date="15th July '233" />
+          <Card title="Best albums" date="15th July '23" />
+        </div>
+      </motion.div>
 
-    <motion.div className='date' initial={{y: 50, opacity:0}} animate={{y: 0, opacity:1}} transition={{duration: .4, ease: [.3,0,.2,1.01], delay: .4}}>&copy;{new Date().getFullYear()}</motion.div>
+      <motion.div
+        className="date"
+        initial={{ y: 50, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.4, ease: [0.3, 0, 0.2, 1.01], delay: 0.4 }}
+      >
+        &copy;{new Date().getFullYear()}
+      </motion.div>
 
-    <div className="logo">
+      <div className="logo">
         <Logo color="#C4C4C4" />
         {/* <Image
           className={styles.logo}
@@ -41,7 +64,7 @@ export default function Home() {
           height={37}
           priority
         /> */}
-    </div>
+      </div>
     </>
-  )
+  );
 }
