b1c08dc591031fcc57e39ada2fd17af1ff77dec2
aeonyuonmiller <aym1@mail.com>
2023-06-17 19:09:45 +0200

    changed viewport margin

--- src/app/Components/Card.jsx
+++ src/app/Components/Card.jsx
@@ -9,8 +9,8 @@ const Card = ({title, day, ordinal, month, year}) => {
       initial={{ opacity:0, scale:0.95 }} 
       whileInView={{ opacity:1, scale:1 }} 
       exit={{ opacity:0, scale:0 }} 
-      viewport={{  margin: "20%", once: false }} 
-      transition={{ type: "spring", stiffness: 400, damping: 10, staggerChildren: 0.1 }}
+      viewport={{  margin: "50%", once: false }} 
+      transition={{ type: "spring", stiffness: 400, damping: 10 }}
       whileHover={{ scale: 1.015 }}
     >
       <div className="content">
