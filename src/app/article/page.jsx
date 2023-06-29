eda2284ef2f30aa8f343af03af4529b3b2e46e7f
aeonyuonmiller <aym1@mail.com>
2023-06-29 10:10:08 +0200

    removed unused

--- src/app/article/page.jsx
+++ src/app/article/page.jsx
@@ -8,25 +8,7 @@ export default function Home() {
   return (
     <AnimatePresence mode="wait">
       <LazyMotion features={domAnimation}>
-        <h1>Mind your cookies.</h1>
-
-        <m.div
-          className="title"
-          initial={{ y: 50, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ duration: 0.4, ease: [0.3, 0, 0.2, 1.01], delay: 0.1 }}
-        >
-          Mind <span>your</span> cookies
-        </m.div>
-
-        <m.div
-          className="copyright"
-          initial={{ y: 50, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ duration: 0.4, ease: [0.3, 0, 0.2, 1.01], delay: 0.4 }}
-        >
-          &copy;{new Date().getFullYear()}
-        </m.div>
+        <m.h1 initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}}>Mind your cookies.</m.h1>
 
         <div className="logo">
           <Logo color="#C4C4C4" />
